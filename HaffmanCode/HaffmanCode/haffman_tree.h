2e9f8ea5ca9a6c4b363feffef03a2345ea8a8e09
Roman Baldaev <roma26baldaev@gmail.com>
2017-06-24 22:12:30 +0700

    Working version, finish!

--- HaffmanCode/HaffmanCode/haffman_tree.h
+++ HaffmanCode/HaffmanCode/haffman_tree.h
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <stdio.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -18,6 +19,7 @@ int count_Symbols ( char *source_File, int *a ) { // Function for calculating sy
 	for ( int i = 0; i < 256; i++ ) {
 		if ( a[i] ) number++;
 	}
+	source.close();
 	return number;
 }
 int index_of_Min ( int * a ) {
@@ -74,6 +76,11 @@ class boolVect {
 				if ( tab[i] ) cout << (char)(i);
 			}
 		}
+		char get_Symbol () {
+			for ( int i = 0; i < 256; i++ ) {
+				if ( tab[i] ) return (char)(i);
+			}
+		}
 };
 
 boolVect & operation_AND ( boolVect & V1, boolVect & V2 ) {
@@ -86,6 +93,8 @@ boolVect & operation_AND ( boolVect & V1, boolVect & V2 ) {
 
 class Node {
 	private:
+		friend class table_Coding;
+		friend class Huffman_Tree;
 		friend Node * create_Sort_List ( int * , char * );
 		friend void Print ( Node *);
 		int frequency;
@@ -93,6 +102,7 @@ class Node {
 		Node *next;
 		Node *left;
 		Node *right;
+		string code;
 	public:
 		Node ( int _freq = 0, Node * _next = NULL, Node * _left = NULL, Node * _right = NULL ) {
 			frequency = _freq;
@@ -100,6 +110,7 @@ class Node {
 			next = _next;
 			left = _left;
 			right = _right;
+			code = "";
 		}
 		int & get_Freq () {
 			return frequency;
@@ -114,6 +125,59 @@ class Node {
 			return right;
 		}
 		
+};
+class Huffman_Tree {
+	private: 
+		Node *root;
+	public: 
+		Huffman_Tree ( Node *r = NULL ) {
+			root = r;
+		}
+		Huffman_Tree ( int * a, char *source_File ) {
+			root = create_Sort_List ( a, source_File );
+		}
+		Node * make_Union_Node ( Node *First, Node *Second ) {
+			Node *new_First = new Node ( First -> get_Freq(), NULL, First -> left, First -> right );
+			Node *new_Second = new Node ( Second -> get_Freq(), NULL, Second -> left, Second -> right );
+			new_First -> V = First -> V;
+			new_Second -> V = Second -> V;
+			int first_Freq = First -> get_Freq ();
+			int second_Freq = Second -> get_Freq ();
+			Node * Union = new Node ( first_Freq + second_Freq, NULL, new_First, new_Second );
+			Union -> V = operation_AND ( First -> V, Second -> V );
+			return Union;
+		}
+		void unite_Nodes () {
+			Node * First = root;
+			Node * Second = First -> next;
+			Node * Union = make_Union_Node ( First, Second );
+			Node * Position = First -> next;
+			while ( ( Position -> next ) && ( ( Position -> next -> get_Freq() ) < ( Union -> get_Freq() ) ) ) {
+				Position = Position -> next;
+			}
+			if ( Position -> next == NULL ) {
+				Position -> next = Union;
+			}
+			else {
+				Node * next_Position = Position -> next;
+				Position -> next = Union;
+				Union -> next = next_Position;
+			}
+			root = Second -> next;
+			delete First;
+			delete Second;
+		}
+		Node * get_Root () {
+			return root;
+		}
+		void make_Huffman_Tree ( ) {
+			while ( root -> next -> next ) {
+				unite_Nodes ();
+				Print ( root );
+				cout << "//////////////////////////////" << endl;
+			}
+			root = make_Union_Node ( root, root -> next );
+		}
 };
 void Print ( Node *Sort) {
 			Node *P = Sort;
@@ -137,5 +201,49 @@ Node * create_Sort_List ( int * a, char *source_File ) {
 		a[index_of_Min(a)] = 0;
 		Copy = Copy -> get_Next();
 	}
+	
 	return Root;
 }
+
+class table_Coding {
+	private:
+		int n, position;
+		char * Tab;
+		string * tab;
+	public:
+		table_Coding ( int _n, Node * R ) {
+			position = 0;
+			n = _n;
+			Tab = new char [n];
+			tab = new string [n];
+			formation_Table ( R );
+		}
+		void formation_Table ( Node * R ) {
+			if ( position == n  ) return; 
+			if ( ( R -> left == NULL ) && ( R -> right == NULL ) ) {
+				Tab [position] = R->V.get_Symbol();
+				tab [position] = R->code;
+				position++;
+			}
+			else {
+				R->left->code = R -> code + "0";
+				R->right->code = R -> code + "1";
+			}
+			if ( R -> left ) formation_Table ( R->left );
+			 if ( R -> right ) formation_Table ( R->right );
+		}
+		void Print () {
+			cout << "Table coding: " << endl;
+			for ( int i = 0; i < n; i++ ) {
+				cout << Tab [i] << ": " << tab[i] << endl;
+			}
+		}
+		int find_Symbol ( char s ) {
+			for ( int i = 0; i < n; i++ ) {
+				if ( Tab [i] == s ) return i;
+			}
+		}
+		string * get_Str_Tab () {
+			return tab;
+		}
+};
--- HaffmanCode/HaffmanCode/main.cpp
+++ HaffmanCode/HaffmanCode/main.cpp
@@ -1,19 +1,49 @@
 #include "haffman_tree.h"
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 void main () {
-	int *a;
+	int *a, *copy, n = 0, source_summ = 0, code_summ = 0;
 	a = new int [256];
+	copy = new int [256];
 	count_Symbols ( "source.txt", a );
+	for ( int i = 0; i < 256; i++ ) {
+		copy [i] = a [i];
+	}
 	for ( int i = 0; i < 256; i++ ) {
 		if ( a[i] ) {
 			cout << (char)(i) << ": " << a[i] << endl;
+			n++;
+			source_summ += a[i];
 		}
 	}
-	Node * R = create_Sort_List ( a, "source.txt" );
-	Print ( R );
+	Huffman_Tree A ( a, "source.txt" );
+	Print ( A.get_Root() );
+	cout << "//////////////////////////////" << endl;
+	A.make_Huffman_Tree();
+	Print ( A.get_Root() );
+	table_Coding Table ( n, A.get_Root() );
+	for ( int i = 0; i < 256; i++ ) {
+		if ( copy[i] ) {
+			int k = Table.find_Symbol((char)(i));
+			code_summ += copy[i] * Table.get_Str_Tab()[k].size();
+		}
+	}
+	Table.Print();
+	cout << "Source size: " << source_summ * 8 << " byte" << endl;
+	cout << "Code size: " << code_summ << " byte" << endl;
+	cout << "Compression ratio: " << (float)((code_summ)/(source_summ * 8.0)) * 100 << "%" << endl;
+
+	fstream source, code_output;
+	source.open( "source.txt", ios::in );
+	code_output.open( "code_output.txt", ios::out );
+	while ( source ) {
+		int ch = source.get();
+		int k = Table.find_Symbol((char)(ch));
+		if ( k < n ) code_output << Table.get_Str_Tab()[k];
+	}
 }
 
